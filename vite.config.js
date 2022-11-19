29c95036cbe0437e01f4f8e2930c5d68452feba5
d.sosinsky <d.sosinsky@duotek.ru>
2022-11-19 04:00:42 +0200

    changes the port

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [svelte()],
-    // server: {
-    //     port: 1001
-    // }
+    server: {
+        port: 1024
+    }
 })
